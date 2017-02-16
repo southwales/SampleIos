69f981d204f1159ec33cc5b36e867f47697696e4
pramodh <pramodh442@gmail.com>
2017-02-16 20:32:38 +0530

    Initial Commit

---
+++ SampleIos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleIos
+//
+//  Created by Naren Datta Raparthi on 16/02/17.
+//  Copyright © 2017 DEV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleIos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleIos
+//
+//  Created by Naren Datta Raparthi on 16/02/17.
+//  Copyright © 2017 DEV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
